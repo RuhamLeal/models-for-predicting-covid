f91209613f0242664db5de6905f0bf7e4686e743
Ruham Leal <ruhamxlpro@hotmail.com>
2022-10-10 01:10:38 -0300

    chore(#2): adding build to manipulate the data

--- index.py
+++ index.py
@@ -7,3 +7,16 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 import re
+
+url = 'https://github.com/neylsoncrepalde/projeto_eda_covid/blob/master/covid_19_data.csv?raw=true'
+df = pd.read_csv(url, parse_dates=['ObservationDate', 'Last Update'])
+
+def fix_columns(column_name):
+	return re.sub(r"[/| ]", "", column_name).lower()
+
+df.columns = [ fix_columns(column) for column in df.columns ]
+
+brazil = df.loc[
+	(df.countryregion == 'Brazil') &
+	(df.confirmed > 0)
+]
